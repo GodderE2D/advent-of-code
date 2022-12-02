f1de3ce39beeb2ef05646776b67f95a07c0140dd
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2022-12-02 14:55:00 -0500

    Create part-2.ts

---
+++ results/2022/day-2/part-2.ts
@@ -0,0 +1,63 @@
+import { readFileSync } from "fs";
+
+// If you did not clone the Git repo, replace the value with your AoC's input or copy and paste it from ./input.txt.
+const input = readFileSync("./input.txt");
+
+const scoreMap = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+};
+
+const rounds = input.split("\n").map((r) => r.split(" "));
+let score = 0;
+
+for (const round of rounds) {
+    let result: 6 | 0 | 3;
+    let roundResult: "X" | "Y" | "Z";
+
+    if (round[1] === "X") {
+        result = 0;
+    } else if (round[1] === "Y") {
+        result = 3;
+    } else if (round[1] === "Z") {
+        result = 6;
+    } else {
+        throw new Error("Invalid result.");
+    }
+
+    if (round[0] === "A" && round[1] === "X") {
+        result = 0;
+        roundResult = "Z";
+    } else if (round[0] === "A" && round[1] === "Y") {
+        result = 3;
+        roundResult = "X";
+    } else if (round[0] === "A" && round[1] === "Z") {
+        result = 6;
+        roundResult = "Y";
+    } else if (round[0] === "B" && round[1] === "X") {
+        result = 0;
+        roundResult = "X";
+    } else if (round[0] === "B" && round[1] === "Y") {
+        result = 3;
+        roundResult = "Y";
+    } else if (round[0] === "B" && round[1] === "Z") {
+        result = 6;
+        roundResult = "Z";
+    } else if (round[0] === "C" && round[1] === "X") {
+        result = 0;
+        roundResult = "Y";
+    } else if (round[0] === "C" && round[1] === "Y") {
+        result = 3;
+        roundResult = "Z";
+    } else if (round[0] === "C" && round[1] === "Z") {
+        result = 6;
+        roundResult = "X";
+    } else {
+        throw new Error("Invalid result.");
+    }
+
+    score += scoreMap[roundResult] + result;
+}
+
+console.log(score);
