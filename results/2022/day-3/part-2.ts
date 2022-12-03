7630c5c3dc4f42f611dce95c67d205370b3dc62b
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2022-12-03 18:41:03 -0500

    Add day 3 input instructions

--- results/2022/day-3/part-2.ts
+++ results/2022/day-3/part-2.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "fs";
 
+// If you did not clone the Git repo, replace the value with your AoC's input or copy and paste it from ./input.txt.
 const input = readFileSync("./input.txt", "utf-8");
 
 const priorityMap = new Map([
