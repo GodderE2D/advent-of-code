cf35347bf8dccc6d3a64513853a37716051846a4
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2022-12-06 08:50:35 -0500

    Day 6 part 1

---
+++ results/2022/day-6/part-1.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+
+// If you did not clone the Git repo, replace the value with your AoC's input or copy and paste it from ./input.txt.
+const input = readFileSync("./input.txt", "utf-8");
+
+const marker = 14;
+const letters = input.split("");
+
+let charactersProcessed = 0;
+
+for (const [i, letter] of letters.entries()) {
+    charactersProcessed++;
+    if (i < marker) continue;
+    const lastFourLetters: string[] = [];
+
+    for (let lastLetterCount = 1; lastLetterCount <= marker; lastLetterCount++) {
+        lastFourLetters.push(letters[i - lastLetterCount]);
+    }
+
+    if (new Set(lastFourLetters).size === marker) break;
+}
+
+console.log(charactersProcessed - 1);
