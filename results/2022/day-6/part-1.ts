d2526360dd05beb540414719ec1232c97f6775f2
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2022-12-06 08:51:15 -0500

    Day 6, fix marker constant

--- results/2022/day-6/part-1.ts
+++ results/2022/day-6/part-1.ts
@@ -3,7 +3,7 @@ import { readFileSync } from "fs";
 // If you did not clone the Git repo, replace the value with your AoC's input or copy and paste it from ./input.txt.
 const input = readFileSync("./input.txt", "utf-8");
 
-const marker = 14;
+const marker = 4;
 const letters = input.split("");
 
 let charactersProcessed = 0;
